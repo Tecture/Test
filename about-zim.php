88642de9cf699d505586181446cddc43edbcaebc
Sudhir Deshmukh <tectureindia@gmail.com>
2017-12-19 14:52:02 +0530

    Add files via upload

---
+++ about-zim.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>.async-hide { opacity: 0 !important} </style>
+<script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+})(window,document.documentElement,'async-hide','dataLayer',4000,
+{'GTM-K4X7LZJ':true});</script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M99NKLL');</script>
+<!-- End Google Tag Manager -->
+<title>ZIM Laboratories Limited - About Us</title>
+<link rel="SHORTCUT ICON" href="images/logo.png" />
+<meta name="author" content="tecture.in">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="We are the best pharmaceutical comapny who manufacture Pre-formulation Intermediate, Finished formulation,
+Orally Disintegrating Strips exporter.">
+<meta name="keywords" content="pellets manufacturing companies in nagpur,orally disintegrating strips,ods manufacturers in india,
+orally disintegrating strips manufacturers in india,pharma companies in nagpur,oral thin film,
+pellets manufacturers in nagpur,Pellets India,pellets manufacturing,orlistat pellets,
+EU-GMP certified company,EU-GMP certified companies in india,pellets manufacturer companies in nagpur,
+tamsulosin pellets, fluconazole pellets,duloxetine hcl ec pellets,potassium hydroxide pellets manufacturer.
+orlistat pelletscaustic pellet, caustic pellet manufacturers, ambroxol pellets,bio cemetery,monosol rx pharm film,oral soluble film,Tadalafil oral disintegrating film,ondansetron film
+methylcobalamin film,vitamin d3, Cholecalciferol,Biotechnology and generic,levocetirizine film,Montelukast film,Rizatriptan oral film,oral soluble film,sildenafil film,mouth freshener film,thin oral film,Ziminta
+">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/styles.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link href="tipuesearch/tipuesearch.css" rel="stylesheet">
+<script src="tipuesearch/tipuesearch_set.js"></script>
+<script src="tipuesearch/tipuesearch.min.js"></script>
+<script src="js/functions.js"></script>
+<script>
+$(document).ready(function(){	  
+$("#menu-about").addClass("active");
+
+var oldSrc = 'images/milestones.png';
+var newSrc = 'images/milestones_vertical.png';
+
+var device_change = function() { 
+
+	if (window.matchMedia('(max-width: 425px)').matches)
+	{
+		$('img[src="' + oldSrc + '"]').attr('src', newSrc);
+	} 
+	else {
+		$('img[src="' + newSrc + '"]').attr('src', oldSrc);
+	}
+
+};
+$(window).resize(device_change);
+device_change(); 
+});
+</script>
+<?php include("phpcommon/pace.php"); ?>  
+</head>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M99NKLL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<?php include_once("phpcommon/analyticstracking.php") ?>
+<style>.async-hide { opacity: 0 !important} </style>
+<script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+})(window,document.documentElement,'async-hide','dataLayer',4000,
+{'GTM-K4X7LZJ':true});</script>
+
+<div class="container-fluid wrapper">
+    <?php include("phpcommon/menu.php"); ?>
+	<div class="zim-content">
+
+	<!-- Wrapper for slides -->
+	<div class="container-fluid bg">
+		<img src="images/about-banner2.jpg" alt="Image" class="img-responsive">
+	</div>
+	
+	<div class="container-fluid about-content sidebar-content">
+		<div class="row">
+			<div class="col-sm-2">
+				<ul class="nav nav-pills nav-stacked nav-side">
+					<li class="active"><a href="about-zim.php">About ZIM</a></li>
+					<li><a href="vision-mission-values.php">Vision, Mission &amp; Values</a></li>
+					<li><a href="corp-info.php">Corporate Information</a></li>
+					<li><a href="investors-relations.php">Investor Relations</a></li>
+					<li><a href="institutional-business.php">Institutional Business</a></li>
+					<li><a href="management.php">Management</a></li>
+					<li><a href="csr.php">Corporate Social Responsibility</a></li>
+				</ul>
+				
+			</div>
+			<div class="col-sm-10">
+				<div class="tab-content">
+					<div id="section1" class="tab-pane fade in active zim-search-desc">
+						<h2>ZIM at a glance</h2>
+						<!--<p>Incepted in 1989, ZIM has made a significant mark as a maverick pharma innovator. Throughout all these years constantly penetrating newer areas of pharmaceutical technology and business, ZIM has established itself as a preferred healthcare solution provider in the field of New Drug Delivery Systems.</p>
+						<p>Headquartered in the Industrial belt of Nagpur city of central India, we are an EU-GMP/WHO-GMP Certified and ISO 9001:2008 accredited company supported by an excellent team of professionals and the capacity to meet large requirements for different market segments. We are engaged in the activities of research and development, manufacturing, distribution and marketing of oral solid dosage forms such as tablets, capsules, pellets, multi-particulate systems, dry syrups, directly compressible granules, taste masked powders, granules and orally disintegrating strips.</p>-->
+						<p>We are an innovative drug delivery solutions providing company targeting patient convenience and adherence. We use proprietary technologies (modified release and taste masking), to develop and manufacture differentiated pharmaceutical products.</p> 
+						<p>Located in Nagpur, Central India, we are an EU-GMP, WHO-GMP Certified and ISO 9001:2008 accredited company supported by an excellent team of professionals and state-of-the-art facility. We are engaged in the activities of research and development, manufacturing, distribution and marketing of oral solid dosage formulations and pre-formulations (PFI) covering tablets, capsules, dry syrups, pellets, granules and orally disintegrating strips (ODS).</p>
+						<p>We are driven by a need for constant innovation and a zeal for excellence. We innovate on and deliver unique drug delivery solutions resulting in niche products. These products, characterized by their technical complexity and market differentiation, offer a unique positioning to deliver enhanced product attributes to patients.</p>
+						<p>We aspire to play a key role in making the benefits of innovative technologies available to our customers as affordable solutions. We partner with like-minded business associates while respecting the intellectual rights of others.</p> 
+						<p>We are present in diverse, high-growth, niche markets, both functionally and geographically, the institutional and pre-formulation intermediate businesses being important ones.</p>
+						<p>We are committed towards providing our customers and business associates value for money through timely access to innovative process technologies and quality products that not only differentiate them from their competitors but also improve their bottom line. The high appreciation from all our associates for our product & service quality continues to inspire us to keep raising our standards.</p>
+						<p>And that's why, every day, we continue to put our zeal for excellence and spirit of innovation into all our actions...</p>
+						<br/>
+						<h2>The ZIM Advantage</h2>
+						<p><ul style="margin-left:20px;">
+						<li>Single window, multiple product / technology offerings.</li>
+						<li>Non-infringing processes , Indian cost advantage.</li>
+						<li>Significant part of the business is generic pharmaceutical manufacturing and distribution (full cycle experience and proof of concept). For instance, we offer Ziminta Mouth Freshener Strips.</li>
+						<li>Fully equipped research centre, prestigious university collaborations. Ability to deliver on co-development–manufacturing out-licensing model.</li>
+						<li>All manufacturing facilities possess multiple accreditations and registrations, including EU-GMP, WHO-GMP etc.</li>
+						</ul></p>
+						<br/>
+						<h2>Our Milestones</h2>
+						<img src="images/milestones.png" alt="" style="margin-bottom:10px;" class="img-responsive"> 
+						<!--<p>At ZIM we believe in constant innovation. We have earned wide appreciation for quality products and prompt services by our business associates.</p>
+						<p>Our business strategy is to innovate on and deliver high-throughput technology based niche products. These products characterized by their technical complexity and market differentiation offer a unique positioning to deliver enhanced product attributes to the patients.</p>
+						<p>ZIM is present in diverse, high-growth, niche markets, both functionally and geographically, the institutional and pre-formulation intermediate businesses being important ones.</p>
+						<p>At ZIM we aspire to play a key role in making some innovative technologies available to our customers as affordable solutions. We partner with like-minded business associates while respecting the intellectual rights of others.</p>
+						<p>We are committed toward providing our customers and business associates value for money by providing timely access to innovative technologies and innovative quality products that not only differentiate them from their competitors but also improve their bottom line.</p>
+						<p>That's why, every day we continue to put our zeal for excellence and spirit of innovation into all our actions.</p>-->
+					</div>
+				</div>				
+			</div>
+		</div>
+	</div>
+	</div>
+	<?php include("phpcommon/footer.php"); ?>
+</div>
+<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer></script>
+</body>
+</html>
+
---
+++ accr.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>.async-hide { opacity: 0 !important} </style>
+<script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+})(window,document.documentElement,'async-hide','dataLayer',4000,
+{'GTM-K4X7LZJ':true});</script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M99NKLL');</script>
+<!-- End Google Tag Manager -->
+<title>ZIM Laboratories Limited - Accreditations</title>
+<link rel="SHORTCUT ICON" href="images/logo.png" />
+<meta name="author" content="tecture.in">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Our facilities are accredited with respected certifications from authorized independent bodies, which are generally accepted worldwide as a mark of approval of the quality of the manufacturer’s operational capabilities">
+<meta name="keywords" content="EU GMP Certification, WHO GMP Certifications for General and Cephalosporin formulation facilities,
+WHO GMP Certification for ODS formulation,Rizatriptan oral film,oral soluble film,sildenafil film,mouth freshener film,thin oral film,Ziminta">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/styles.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link href="tipuesearch/tipuesearch.css" rel="stylesheet">
+<script src="tipuesearch/tipuesearch_set.js"></script>
+<script src="tipuesearch/tipuesearch.min.js"></script>
+<script src="js/functions.js"></script>
+<script>
+$(document).ready(function(){	  
+$("#menu-accr").addClass("active");
+});
+</script>
+<?php include("phpcommon/pace.php"); ?>  
+</head>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M99NKLL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<?php include_once("phpcommon/analyticstracking.php") ?>
+<div class="container-fluid wrapper">
+    <?php include("phpcommon/menu.php"); ?>
+	<div class="zim-content">
+
+	<!-- Wrapper for slides -->
+	<div class="container-fluid bg">
+		<img src="images/accr-banner.jpg" alt="Image" class="img-responsive">
+	</div>
+	
+	<div class="container accr-content zim-search-desc">
+	<h2>Accreditations</h2>
+
+	<p>Our facilities are accredited with respected certifications from authorized independent bodies, which are generally accepted worldwide as a mark of approval of the quality of the manufacturer’s operational capabilities. These accreditations are:</p>
+	<ul style="margin-left:20px;" class="padding-ul">
+	<li><p>EU GMP Certification</p></li>
+	<li><p>WHO GMP Certifications for General and Cephalosporin formulation facilities</p></li>
+	<li><p>WHO GMP Certification for ODS formulation</p></li>
+	<li><p>ISO 9001: 2008 Certification</p></li>
+	</ul>
+	
+	<div class="row row-eq-height">
+	<div class="col-sm-3"><img src="images/certs/cert1.jpg" alt="" class="img-responsive center-block" /></div>
+	<div class="col-sm-3"><img src="images/certs/cert2.jpg" alt="" class="img-responsive center-block" /></div>
+	<div class="col-sm-3"><img src="images/certs/cert3.jpg" alt="" class="img-responsive center-block" /></div>
+	<div class="col-sm-3"><img src="images/certs/cert4.jpg" alt="" class="img-responsive center-block" /></div>
+	</div>
+	<div class="row row-eq-height">
+	<div class="col-sm-3"><img src="images/certs/cert5.jpg" alt="" class="img-responsive center-block" /></div>
+	<div class="col-sm-3"><img src="images/certs/cert6.jpg" alt="" class="img-responsive center-block" /></div>
+	<div class="col-sm-3"><img src="images/certs/cert7.jpg" alt="" class="img-responsive center-block" /></div>
+	<div class="col-sm-3"><img src="images/certs/cert8.jpg" alt="" class="img-responsive center-block" /></div>
+	</div>
+	<div class="row row-eq-height">
+	<div class="col-sm-3"><img src="images/certs/cert9.jpg" alt="" class="img-responsive center-block" /></div>
+	<div class="col-sm-3"><img src="images/certs/cert10.jpg" alt="" class="img-responsive center-block" /></div>
+	<div class="col-sm-3"><img src="images/certs/cert11.jpg" alt="" class="img-responsive center-block" /></div>
+	<div class="col-sm-3"><img src="images/certs/three star.jpg" alt="" class="img-responsive center-block" /></div>
+	</div>
+	<br/>
+	<br/>
+	<h2>Global Presence</h2>
+	<p>ZIM has exclusive supply agreements for complex PFIs with market leaders in various geographies. ZIM respects client confidentiality and welcomes the opportunity to provide specific information after due permission from its valued partners. We already have our products registered in many countries of following regions,</p>
+	<img src="images/map.png" alt="" class="img-responsive" />
+	</div>
+	</div>
+	<?php include("phpcommon/footer.php"); ?>
+</div>
+<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer></script>
+</body>
+</html>
+
